7d49b4efcb7550032499ed46e5c9884c5185b2a7
Othmane Makkour <123239375+OTH-DOT@users.noreply.github.com>
2025-05-13 10:42:21 +0100

    feat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,67 +4,42 @@ import axios from 'axios'
 import Home from "./pages/website/home";
 import Login from "./auth/login";
 import Register from "./auth/register";
+import { Toaster } from "react-hot-toast";
+import Layout from "./layout";
 
-// PrivateRoute component to protect private routes
+// Uncomment and use if you need to protect routes
 // const PrivateRoute = ({ element }) => {
 //   const { token } = useSelector(state => state.auth);  // Access token from Redux store
-
-//   console.log(token)
-//   if (!token) {
-//     return <Navigate to="/login" replace />;  // Redirect to login if not authenticated
-//   }
-
-//   return element;
+//   return token ? element : <Navigate to="/login" replace />;
 // };
 
-// AuthRoute component to prevent logged-in users from accessing auth pages
 // const AuthRoute = ({ element }) => {
-//   const { token , user } = useSelector(state => state.auth);  // Access token from Redux store
-//   console.log(user)
-//   if (token) {
-//     return <Navigate to="/" replace />;  // Redirect to home if already logged in
-//   }
-
-//   return element;
+//   const { token } = useSelector(state => state.auth);
+//   return token ? <Navigate to="/" replace /> : element;
 // };
 
 function App() {
-
   return (
     <>
-      <Toaster
-        position="top-right"
-        reverseOrder={false}
-      />
-    
+      <Toaster position="top-right" reverseOrder={false} />
+      
       <Routes>
-        {/* Auth Routes (only accessible when not authenticated) */}
-        <Route path="/login" element={<AuthRoute element={<Login />} />}/>
-        <Route path="/register" element={<AuthRoute element={<Register />} />} />
-
+        {/* Public route */}
         <Route path="/" element={<Layout />}>
-          <Route index element={<AuthRoute element={<Home />} />} />
-
+          <Route index element={<Home />} />
         </Route>
 
-        {/* <Route path="/" element={<RoleRoute element={<Layout />} allowedRoles={['admin','customer']} />}>
+        {/* Auth routes (uncomment if using auth logic) */}
+        {/* 
+        <Route path="/login" element={<AuthRoute element={<Login />} />} />
+        <Route path="/register" element={<AuthRoute element={<Register />} />} />
+        */}
 
-        </Route>
-
-        <Route path="/admin" element={<PrivateRoute element={<RoleRoute element={<AdminLayout />} allowedRoles={['admin']} />}/>}>
-
-        </Route>
-        
-        <Route path="/manager" element={<PrivateRoute element={<RoleRoute element={<ManagerLayout />} allowedRoles={['admin', 'manager']} />}/>}>
-          
-        </Route> */}
-
-
-        {/* Catch-all route */}
+        {/* Catch-all redirect */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-      </>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
