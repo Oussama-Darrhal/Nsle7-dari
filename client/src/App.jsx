e1f5eecf08a9b5f06ba4184a10c5448051fa0cb7
Othmane Makkour <123239375+OTH-DOT@users.noreply.github.com>
2025-05-13 10:32:40 +0100

    feat: create folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes, BrowserRouter as Router, Navigate } from "react-router-dom";
+// import { useDispatch, useSelector } from 'react-redux';  // Assuming you're using Redux for authentication state
+import axios from 'axios'
+import Home from "./pages/website/home";
+import Login from "./auth/login";
+import Register from "./auth/register";
+
+// PrivateRoute component to protect private routes
+// const PrivateRoute = ({ element }) => {
+//   const { token } = useSelector(state => state.auth);  // Access token from Redux store
+
+//   console.log(token)
+//   if (!token) {
+//     return <Navigate to="/login" replace />;  // Redirect to login if not authenticated
+//   }
+
+//   return element;
+// };
+
+// AuthRoute component to prevent logged-in users from accessing auth pages
+// const AuthRoute = ({ element }) => {
+//   const { token , user } = useSelector(state => state.auth);  // Access token from Redux store
+//   console.log(user)
+//   if (token) {
+//     return <Navigate to="/" replace />;  // Redirect to home if already logged in
+//   }
+
+//   return element;
+// };
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+      />
+    
+      <Routes>
+        {/* Auth Routes (only accessible when not authenticated) */}
+        <Route path="/login" element={<AuthRoute element={<Login />} />}/>
+        <Route path="/register" element={<AuthRoute element={<Register />} />} />
+
+        <Route path="/" element={<Layout />}>
+          <Route index element={<AuthRoute element={<Home />} />} />
+
+        </Route>
+
+        {/* <Route path="/" element={<RoleRoute element={<Layout />} allowedRoles={['admin','customer']} />}>
+
+        </Route>
+
+        <Route path="/admin" element={<PrivateRoute element={<RoleRoute element={<AdminLayout />} allowedRoles={['admin']} />}/>}>
+
+        </Route>
+        
+        <Route path="/manager" element={<PrivateRoute element={<RoleRoute element={<ManagerLayout />} allowedRoles={['admin', 'manager']} />}/>}>
+          
+        </Route> */}
+
+
+        {/* Catch-all route */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+      </>
   )
 }
 
